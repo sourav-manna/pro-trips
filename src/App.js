9a8b17bf397c13f3adc5296c5dbe5a8725fb8d1e
sourav-manna <sourav.manna2000@outlook.com>
2022-07-28 14:12:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideNav from './components/SideNav';
+import Home from './components/Home';
+import CreateTrip from './components/CreateTrip';
+import DisplayTrip from './components/DisplayTrip';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trips: [
+        { date: "2017-01-10", place: "Manali", type: "trek" },
+        { date: "2017-04-05", place: "Mumbai", type: "club" },
+        { date: "2017-02-09", place: "Marina", type: "tropic" },
+        { date: "2020-10-21", place: "Coimbatore", type: "trek" }
+      ]
+    }
+  }
+  addTrip = (newTrips) => {
+    this.setState({
+      trips: [...newTrips]
+    })
+  }
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <SideNav />
+          <div className="workplace">
+            <Routes>
+              <Route exact path="/" element={<Home trips={this.state.trips} />}/>
+              <Route path="/addTrip" element={<CreateTrip addTrip={this.addTrip} trips={this.state.trips} />} />
+              <Route path="/Trips"element={ <DisplayTrip trips={this.state.trips} />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
-
-export default App;
